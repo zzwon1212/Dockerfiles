901f5b4f6d5cb16513cd03cb5f55e190232b33ae
zzwon1212 <zzwon1212@snu.ac.kr>
2025-05-29 10:30:10 +0000

    Update
    Signed-off-by: zzwon1212 <zzwon1212@snu.ac.kr>

--- MIC/run_docker.sh
+++ MIC/run_docker.sh
@@ -6,11 +6,12 @@ docker run -it \
     --gpus all \
     --shm-size=64G \
     --network host \
-    --ipc=host \
+    --ipc host \
+    -e ROS_DOMAIN_ID=77 \
     -e TZ=Asia/Seoul \
     -e TERM=xterm-256color \
-    --name mic_tmp \
-    mic:cuda110_torch107 \
+    --name VI_DEMO_SEG_INDOOR_UDA \
+    mic_humble:cuda117_torch113 \
     /bin/bash
 
 # chown -R 1015:1015 /workspace/demo/ros2_ws
